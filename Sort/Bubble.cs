af1b09328d01052f6aac372324417e1d4ceb8e92
Nikita <37244958+Skuybedin@users.noreply.github.com>
2022-05-04 16:41:26 +0300

    Update Bubble.cs

--- Sort/Bubble.cs
+++ Sort/Bubble.cs
@@ -14,3 +14,26 @@ static void BubbleSort(int[] array)
 		}
 	}
 }
+
+
+// Реализация с оптимизацией
+static void BubbleSort(int[] array)
+{
+	for (int i = array.Length - 1; i > 0; i--) // на какое место будем ставим наибольший элемент
+	{
+		bool flag = false; // false - не было обменов, true - был хотя бы 1 обмен
+		for (int j = 0; j < i; j++) // проходим по не отсортированной последовательности
+		{
+			if (array[j] > array[j + 1])// если порядок элементов неправильный (неубывание)
+			{
+				// меняем местами пары
+				int temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				flag = true; // меняем флажок
+			}
+		}
+		if(flag == false) return; // если значение флага не поменялось, то массив отсортирован. Выходим с функции
+	}
+}
+	
