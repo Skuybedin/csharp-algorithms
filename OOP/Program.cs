7889ced69b3cef60f07477765b3a918bf142f0ae
Nikita <37244958+Skuybedin@users.noreply.github.com>
2022-05-04 17:12:10 +0300

    Update Program.cs

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -27,7 +27,7 @@ namespace Show
     //    public int CompareTo(User other) => Age.CompareTo(other.Age);
     //}
 
-    public class AgeComparer : IComparer<User> // Правило AgeCompare (если мы хотим сравнить Х и Y объект)
+	public class AgeComparer : IComparer<User> // Правило AgeCompare (если мы хотим сравнить Х и Y объект)
 	{
 		public int Compare(User x, User y) => x.Age.CompareTo(y.Age);
 	}
@@ -43,9 +43,9 @@ namespace Show
 	}
 
 	internal class Program
-    {
+    	{
 		static void Original() // Оригинальный IEnumerable
-        {
+        	{
 			User user1 = new User("Nikita", 25);
 			User user2 = new User("Lera", 23);
 
@@ -58,7 +58,7 @@ namespace Show
 		}
 
 		static void DuckType() // Утиная типизация
-        {
+        	{
 			User user1 = new User("Nikita", 25);
 			User user2 = new User("Lera", 23);
 
@@ -88,7 +88,7 @@ namespace Show
 		}
 
 		static void CompareShow()
-        {
+        	{
 			User user1 = new User("Nikita", 25);
 			User user2 = new User("Lera", 23);
 			User user3 = new User("Lera2", 13);
@@ -103,12 +103,13 @@ namespace Show
 				Console.WriteLine(user.Name);
 			}
 		}
-        static void Main(string[] args)
-        {
+		
+        	static void Main(string[] args)
+        	{
 			//Original();
 			//DuckType();
 			//OriginalT();
 			//CompareShow();
 		}
-    }
+    	}
 }
