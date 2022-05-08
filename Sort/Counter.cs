20d1df7585565d3a8f7ce4ec580fd0832e291075
Nikita <37244958+Skuybedin@users.noreply.github.com>
2022-05-08 14:51:38 +0300

    Create Counter.cs

---
+++ Sort/Counter.cs
@@ -0,0 +1,19 @@
+static void CounterSort()
+{
+	int k = 10; // ширина используемого диапазона
+	int[] counts = new int[k];
+	int n = Convert.ToInt32(Console.ReadLine());
+	for (int i = 0; i < n; i++)
+	{
+		int x = Convert.ToInt32(Console.ReadLine());
+		counts[x] = counts[x] + 1; // подсчитываем
+	}
+
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < counts[i]; j++) // counts[i]-количество чисел i
+		{
+			Console.Write(i + " ");
+		}
+	}
+}
